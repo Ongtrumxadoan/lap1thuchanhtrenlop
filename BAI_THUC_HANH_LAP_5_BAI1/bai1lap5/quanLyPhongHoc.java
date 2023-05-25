eb2f2b2af81c372c284a0749c639cf9459a6bff2
Ongtrumxadoan <103736587+Ongtrumxadoan@users.noreply.github.com>
2023-05-25 18:29:29 +0700

    Add files via upload

---
+++ BAI_THUC_HANH_LAP_5_BAI1/bai1lap5/quanLyPhongHoc.java
@@ -0,0 +1,95 @@
+package bai1lap5;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class quanLyPhongHoc {
+
+    Scanner sc = new Scanner(System.in);
+
+    private ArrayList<phongHocSinhVien> danhsach;
+
+    public quanLyPhongHoc() {
+        this.danhsach = new ArrayList<phongHocSinhVien>();
+    }
+
+    public quanLyPhongHoc(ArrayList<phongHocSinhVien> danhsach) {
+        this.danhsach = danhsach;
+    }
+
+    public void themPhongHoc(phongHocSinhVien phsv) {
+        this.danhsach.add(phsv);
+    }
+
+    public void timKiemPhongHoc(String phong) {
+        for (phongHocSinhVien phsv : danhsach) {
+            if (phsv.getMaPhong().indexOf(phong) >= 0) {
+                System.out.println(phsv);
+            }
+        }
+    }
+
+    public void inDanhSachPhongHoc() {
+        for (phongHocSinhVien phsv : danhsach) {
+            System.out.println(phsv);
+        }
+    }
+    
+    public void xoaMaPhong(){
+        System.out.println("==========XOA PHONG HOC============");
+        System.out.println("nhap ma phong can xoa: ");
+        int maPhongHoc = sc.nextInt();
+        
+        for(phongHocSinhVien phsv : danhsach){
+            if(phsv.getMaPhong().equals(maPhongHoc)){
+                danhsach.remove(phsv);
+            }
+        }
+    }
+    public void sapXepDanhSachCotDayNha() {
+        System.out.println("SAP XEP DANH COT DAY NHA TANG DAN");
+        Collections.sort(danhsach, new Comparator<phongHocSinhVien>() {
+
+            @Override
+            public int compare(phongHocSinhVien s1, phongHocSinhVien s2) {
+                return s1.getDayNha().compareTo(s2.getDayNha());
+            }
+        });
+        inDanhSachPhongHoc();
+    }
+
+    public void sapXepDanhSachGiamDanDienTich() {
+        System.out.println("SAP XEP DANH SACH GIM DAN THEO DIEN TICH");
+        Collections.sort(danhsach, new Comparator<phongHocSinhVien>() {
+
+            @Override
+            public int compare(phongHocSinhVien s1, phongHocSinhVien s2) {
+                return Double.compare(s2.getDienTich(), s1.getDienTich());
+            }
+        });
+        inDanhSachPhongHoc();
+    }
+
+    public void sapXepDanhSachTangDanSoBongDen() {
+        System.out.println("SAP XEP DANH SACH TANG DAN SO BONG DEN");
+        Collections.sort(danhsach, new Comparator<phongHocSinhVien>() {
+
+            @Override
+            public int compare(phongHocSinhVien s1, phongHocSinhVien s2) {
+                return Integer.compare(s1.getSoBongDen(), s2.getSoBongDen());
+            }
+        });
+
+    }
+
+    public int tongDanhSachCacMayTrongPhong() {
+         return this.danhsach.size();
+    }
+
+    public void kiemTraSoMayTrongPhong() {
+
+    }
+}
